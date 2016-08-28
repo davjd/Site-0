a8dc24a9283d9b8718ed5a01060f7fa1831dd57c
dmedinaASC <davidbepic@gmail.com>
2016-08-28 19:47:27 -0400

    Add files via upload
    Edited the homepage a little more.

--- js/1.js
+++ js/1.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
+    $("#img1").fadeOut(1500);
+    $("#img1").fadeIn(2300);
     $("#b1").mouseenter(function() {
-        $("#p1").css("transition", "color 3s");
-        $("#p1").css("color", "yellow");
+        $("#p1").css({
+            "transition": "color 3s, background-color 3s",
+            "color": "rgb(84,178,169)",
+            "background-color": "rgba(0, 51, 51, .15)"
+        });
+        $("#b1").css({
+          "background-color": "rgba(0, 85, 85, .05)"
+        })
     });
     $("#b1").mouseleave(function() {
-        $("#p1").css("color", "rgba(174,140,140,.5)");
+        $("#p1").css({
+            "color": "rgba(174,140,140,.5)",
+            "background-color": "rgba(0, 51, 51, .5)"
+        });
     });
 })
